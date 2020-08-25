48b815d33ffb7ea9314cc97d96fc4c7d70506f45
Stephen <stephen_h369@live.co.uk>
2020-08-25 14:09:22 +0100

    add and remove passengers from bus stop pass

--- src/main/java/BusStop.java
+++ src/main/java/BusStop.java
@@ -1,2 +1,28 @@
+import java.util.ArrayList;
+
 public class BusStop {
+
+    private String name;
+    private ArrayList<Person> queue;
+
+    public BusStop(String name) {
+        this.name = name;
+        this.queue = new ArrayList<Person>();
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int queueLength() {
+        return this.queue.size();
+    }
+
+    public void addPassenger(Person passenger) {
+        this.queue.add(passenger);
+    }
+
+    public void removePassenger(Person passenger) {
+        this.queue.remove(passenger);
+    }
 }
--- src/test/java/BusStopTest.java
+++ src/test/java/BusStopTest.java
@@ -1,2 +1,40 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class BusStopTest {
+
+    private BusStop stop;
+    private Person person1;
+
+    @Before
+    public void before() {
+        stop = new BusStop("City Centre");
+        person1 = new Person();
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("City Centre", stop.getName());
+    }
+
+    @Test
+    public void queueStartsEmpty() {
+        assertEquals(0, stop.queueLength());
+    }
+
+    @Test
+    public void addPassengerToQueue() {
+        stop.addPassenger(person1);
+        assertEquals(1, stop.queueLength());
+    }
+
+    @Test
+    public void removePassengerFromQueue() {
+        stop.addPassenger(person1);
+        stop.removePassenger(person1);
+        assertEquals(0, stop.queueLength());
+    }
+
 }
